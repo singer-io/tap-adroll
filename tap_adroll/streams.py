cb0a6c98342b77964e795f4c179501e0f7bb845f
Kyle Allan <KAllan357@gmail.com>
2020-06-10 14:03:53 -0400

    Add/sync ads (#5)
    * Circle ci now runs json validator and pylint
    
    * wip pylint errors
    
    * Add MANIFEST and include schemas in package data
    
    * Remove tests folder from pylint
    
    * Fixed tap-tester tests to pass now
    
    * Created stream Ads
    
    * Ads JSON schema
    
    * Fix full replication test to check sync correctly
    
    It actually did have data, but wasn't properly selecting the streams. And
    that is because we were not running check mode first before we ran sync.
    
    Co-authored-by: Asaf Erlich <aerlich@talend.com>

--- tap_adroll/streams.py
+++ tap_adroll/streams.py
@@ -15,14 +15,43 @@ class Advertisables:
         self.config = config
         self.state = state
 
+    def get_all_advertisable_eids(self):
+        records = self.client.get(self.endpoint)
+        for rec in records.get('results'):
+            yield rec['eid']
 
     def sync(self):
         records = self.client.get(self.endpoint)
         for rec in records.get('results'):
             yield rec
 
-        # singer.write_state(self.state)
+class Ads:
+    stream_id = 'Ads'
+    stream_name = 'ads'
+    endpoint = 'advertisable/get_ads'
+    key_properties = ["eid"]
+    replication_method = "FULL_TABLE"
+    replication_keys = []
+
+    def __init__(self, client, config, state):
+        self.client = client
+        self.config = config
+        self.state = state
+
+    def sync(self):
+        advertisables = Advertisables(self.client, self.config, self.state)
+        for advertisable_eid in advertisables.get_all_advertisable_eids():
+            records = self.client.get(self.endpoint, params={
+                'advertisable': advertisable_eid
+            })
+            for rec in records.get('results'):
+                yield rec
+
+class AdReports:
+    key_properties = ["eid", "date"]
+
 
 STREAM_OBJECTS = {
     'advertisables': Advertisables,
+    'ads': Ads,
 }
