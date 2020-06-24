284b2554ed838b12de9ec13da208d36bfd93a1f0
Kyle Speer <kspeer@stitchdata.com>
2020-06-24 11:21:26 -0400

    fix indentation

--- tests/base.py
+++ tests/base.py
@@ -171,7 +171,7 @@ class TestAdrollBase(unittest.TestCase):
             )
 
     def _get_abs_path(self, path):
-    return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
+        return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
 
     def _load_schemas(self, stream):
         schemas = {}
