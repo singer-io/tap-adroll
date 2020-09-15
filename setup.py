7110df6556e22c4c0969f166490ec3c63baf89ce
Andy Lu <andy@stitchdata.com>
2020-09-15 15:19:56 +0000

    Bump to v1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-adroll',
-      version='0.0.1',
+      version='1.0.0',
       description='Singer.io tap for extracting data from Adroll',
       author='Stitch',
       url='https://singer.io',
