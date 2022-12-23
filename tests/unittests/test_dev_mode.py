c3cf5f8ae775bacdf867c6d5b5f8e45520b97f9a
“rdeshmukh15” <rdeshmukh@talend.com>
2022-12-23 11:30:41 +0000

    formatated uniit test file

--- tests/unittests/test_dev_mode.py
+++ tests/unittests/test_dev_mode.py
@@ -9,42 +9,51 @@ from tap_adroll.client import AdrollClient
 
 
 class Test_ClientDevMode(unittest.TestCase):
+
     """Test the dev mode functionality."""
+
     def setUp(self):
         """Creates a sample config for test execution"""
 
         # Data to be written
         self.mock_config = {
-                "access_token": "sample_access_token",
-                "refresh_token": "sample_refresh_token",
-                "client_id": "sample_client_id",
-                "client_secret": "sample_client_secret"
-                }
+            "access_token": "sample_access_token",
+            "refresh_token": "sample_refresh_token",
+            "client_id": "sample_client_id",
+            "client_secret": "sample_client_secret",
+        }
         self.tmp_config_filename = "adroll_config.json"
 
         self.token = {
-                "access_token": self.mock_config["access_token"],
-                "refresh_token": self.mock_config["refresh_token"], 
-                "token_type": "Bearer", 
-                "expires_in": "-30"
-                }
+            "access_token": self.mock_config["access_token"],
+            "refresh_token": self.mock_config["refresh_token"],
+            "token_type": "Bearer",
+            "expires_in": "-30",
+        }
         self.extra = {
-                "client_id": self.mock_config["client_id"],
-                "client_secret": self.mock_config["client_secret"]
-                }
+            "client_id": self.mock_config["client_id"],
+            "client_secret": self.mock_config["client_secret"],
+        }
+
         # Serializing json
         json_object = json.dumps(self.mock_config, indent=4)
-        
+
         # Writing to sample.json
         with open(self.tmp_config_filename, "w") as outfile:
             outfile.write(json_object)
 
     def tearDown(self):
         """Deletes the sample config"""
+
         if os.path.isfile(self.tmp_config_filename):
             os.remove(self.tmp_config_filename)
 
-    @patch("requests.Session.request", return_value=MagicMock(return_value={'results': {'eid': 12345}}, status_code=200))
+    @patch(
+        "requests.Session.request",
+        return_value=MagicMock(
+            return_value={"results": {"eid": 12345}}, status_code=200
+        ),
+    )
     def test_client_with_dev_mode(self, mock_request):
         """Checks the dev mode implementation works with existing token"""
 
@@ -53,27 +62,42 @@ class Test_ClientDevMode(unittest.TestCase):
 
         headers = {"Authorization": f"Bearer {self.mock_config['access_token']}"}
 
-        mock_request.assert_called_with("GET", "https://services.adroll.com/api/v1/organization/get", headers=headers, params=None, data=None)
+        mock_request.assert_called_with(
+            "GET",
+            "https://services.adroll.com/api/v1/organization/get",
+            headers=headers,
+            params=None,
+            data=None,
+        )
 
-    @patch("requests_oauthlib.OAuth2Session.request", return_value=MagicMock(return_value={'results': {'eid': 12345}}, status_code=200))
+    @patch(
+        "requests_oauthlib.OAuth2Session.request",
+        return_value=MagicMock(
+            return_value={"results": {"eid": 12345}}, status_code=200
+        ),
+    )
     def test_client_without_dev_mode(self, mock_request):
         """Checking the code flow without dev mode"""
 
         client = AdrollClient(self.tmp_config_filename, self.mock_config, False)
         client.authenticate_request(self.token, self.extra)
 
-        mock_request.assert_called_with("GET", "https://services.adroll.com/api/v1/organization/get", headers=None, params=None, data=None)
+        mock_request.assert_called_with(
+            "GET",
+            "https://services.adroll.com/api/v1/organization/get",
+            headers=None,
+            params=None,
+            data=None,
+        )
 
-    @patch('tap_adroll.client.requests.Session.request')
-    @patch('tap_adroll.client.AdrollClient.get')
+    @patch("tap_adroll.client.requests.Session.request")
+    @patch("tap_adroll.client.AdrollClient.get")
     def test_request_timeout_and_backoff_on_make_request(self, mock_get, mock_send):
-        """
-        Check whether the request backoffs properly for 3 times in case of HTTPError.
-        """
+        """Check whether the request backoffs properly for 3 times in case of HTTPError."""
+
         mock_send.side_effect = HTTPError
         client = AdrollClient(self.tmp_config_filename, self.mock_config, True)
 
         with self.assertRaises(HTTPError):
-            client._make_request('GET', 'organization/get')
+            client._make_request("GET", "organization/get")
         self.assertEquals(mock_send.call_count, 3)
-        
